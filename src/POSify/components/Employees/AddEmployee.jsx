ea8fb96994d4be9f1179e8b2860079769981a8c9
Pablo David Puac García <pablopuacg@gmail.com>
2023-11-05 17:53:33 -0600

    pagina empleados

--- src/POSify/components/Employees/AddEmployee.jsx
+++ src/POSify/components/Employees/AddEmployee.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'tailwindcss/tailwind.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { PosifyLayout } from '../../../layout/PosifyLayout';
@@ -26,6 +27,7 @@ class AgregarEmpleado extends Component {
     this.setState({
       nombre: '',
       apellido: '',
+      puesto:'',
       fecha_nac: null,
     });
   };
@@ -33,36 +35,113 @@ class AgregarEmpleado extends Component {
   render() {
     return (
       <PosifyLayout>
-        <div className="flex w-72 my-10 mx-5 flex-col gap-6">
-          <form onSubmit={this.handleSubmit}>
-            <Input
-              name="nombre"
-              value={this.state.nombre}
-              onChange={this.handleChange}
-              variant="standard" 
-              placeholder="Nombre" 
-            />
-            <Input
-              name="apellido"
-              value={this.state.apellido}
-              onChange={this.handleChange}
-              variant="standard" 
-              placeholder="Apellido" 
-            />
-            <DatePicker
-              name = 'fecha de nacimiento'
-              selected={this.state.fecha_nac}
-              onChange={date => this.setState({ fecha_nac: date })}
-              dateFormat="dd/MM/yyyy"
-              showYearDropdown
-              scrollableYearDropdown
-              yearDropdownItemNumber={100}
-              className='w-200 my-5 border-gray-300 rounded'
-            />
-            <Button type="submit" color="black" ripple="light" className='my-5'>
-              Agregar Empleado
-            </Button>
-          </form>
+        <div className="flex w-72 flex-col gap-6">
+            <form onSubmit={this.handleSubmit}>
+              <div class="relative h-11 w-full min-w-[200px] my-10 mx-7">
+                <input
+                  name="nombre"
+                  value={this.state.nombre}
+                  onChange={this.handleChange}
+                  placeholder="Nombre"
+                  class="peer h-full w-full border-b border-blue-gray-200 
+                        bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal 
+                        text-blue-gray-700 outline outline-0 transition-all 
+                        placeholder-shown:border-blue-gray-200 focus:border-indigo-500 
+                        focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                />
+                <label class="after:content[' '] pointer-events-none absolute left-0 
+                              -top-2.5 flex h-full w-full select-none text-sm font-normal 
+                              leading-tight text-blue-gray-500 transition-all after:absolute 
+                              after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 
+                              after:border-indigo-500 after:transition-transform after:duration-300 
+                              peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 
+                              peer-focus:text-sm peer-focus:leading-tight peer-focus:text-indigo-500 
+                              peer-focus:after:scale-x-100 peer-focus:after:border-indigo-500 peer-disabled:text-transparent 
+                              peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                  Nombre
+                </label>
+              </div>
+              <div class="relative h-11 w-full min-w-[200px] my-10 mx-7">
+                <input
+                  name="apellido"
+                  value={this.state.apellido}
+                  onChange={this.handleChange}
+                  placeholder="Apellido"
+                  class="peer h-full w-full border-b border-blue-gray-200 
+                        bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal 
+                        text-blue-gray-700 outline outline-0 transition-all 
+                        placeholder-shown:border-blue-gray-200 focus:border-indigo-500 
+                        focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  />
+                <label class="after:content[' '] pointer-events-none absolute left-0 
+                              -top-2.5 flex h-full w-full select-none text-sm font-normal 
+                              leading-tight text-blue-gray-500 transition-all after:absolute 
+                              after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 
+                              after:border-indigo-500 after:transition-transform after:duration-300 
+                              peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 
+                              peer-focus:text-sm peer-focus:leading-tight peer-focus:text-indigo-500 
+                              peer-focus:after:scale-x-100 peer-focus:after:border-indigo-500 peer-disabled:text-transparent 
+                              peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Apellido
+                </label>
+              </div>
+              <div class="relative h-11 w-full min-w-[200px] my-10 mx-7">
+                <input
+                  name="puesto"
+                  value={this.state.puesto}
+                  onChange={this.handleChange}
+                  placeholder="Puesto"
+                  class="peer h-full w-full border-b border-blue-gray-200 
+                        bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal 
+                        text-blue-gray-700 outline outline-0 transition-all 
+                        placeholder-shown:border-blue-gray-200 focus:border-indigo-500 
+                        focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  />
+                <label class="after:content[' '] pointer-events-none absolute left-0 
+                              -top-2.5 flex h-full w-full select-none text-sm font-normal 
+                              leading-tight text-blue-gray-500 transition-all after:absolute 
+                              after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 
+                              after:border-indigo-500 after:transition-transform after:duration-300 
+                              peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 
+                              peer-focus:text-sm peer-focus:leading-tight peer-focus:text-indigo-500 
+                              peer-focus:after:scale-x-100 peer-focus:after:border-indigo-500 peer-disabled:text-transparent 
+                              peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Puesto
+                </label>
+              </div>
+              <div class="relative h-11 w-full min-w-[200px] my-10 mx-7">
+              <DatePicker
+                name = 'fecha de nacimiento'
+                selected={this.state.fecha_nac}
+                onChange={date => this.setState({ fecha_nac: date })}
+                dateFormat="dd/MM/yyyy"
+                showYearDropdown
+                scrollableYearDropdown
+                yearDropdownItemNumber={100}
+                className="my-5 peer h-full w-full rounded-[7px] border border-blue-gray-200  
+                          bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 
+                          transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 
+                          placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-purple-500 
+                          focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+              />
+              <label class="after:content[' '] pointer-events-none absolute left-0 
+                              -top-2.5 flex h-full w-full select-none text-sm font-normal 
+                              leading-tight text-blue-gray-500 transition-all after:absolute 
+                              after:-bottom-2.5 after:block after:w-full after:scale-x-0 after:border-b-2 
+                              after:border-indigo-500 after:transition-transform after:duration-300 
+                              peer-placeholder-shown:leading-tight peer-placeholder-shown:text-blue-gray-500 
+                              peer-focus:text-sm peer-focus:leading-tight peer-focus:text-indigo-500 
+                              peer-focus:after:scale-x-100 peer-focus:after:border-indigo-500 peer-disabled:text-transparent 
+                              peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Fecha de nacimiento
+              </label>
+              </div>
+              
+              <Button type="submit" color="indigo" className=' mx-7'>
+                Agregar Empleado
+              </Button>
+              
+            </form>
         </div>
       </PosifyLayout>
     );
--- src/POSify/components/Employees/ListEmployee.jsx
+++ src/POSify/components/Employees/ListEmployee.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import AgregarEmpleado from './AddEmployee';
+import { Pagination } from '../Pagination';
+import { Input, Button } from "@material-tailwind/react";
 class ListEmployee extends Component {
   state = {
     empleados: [],
     empleadosSeleccionados: {},
+    currentPage: 1, // Página actual
+    itemsPerPage: 10, // Cantidad de elementos por página
+    searchTerm: '', // Término de búsqueda
   };
 
   handleEliminarEmpleado = (index) => {
@@ -34,22 +39,53 @@ class ListEmployee extends Component {
     });
   };
 
+  handleSearch = (e) => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  setCurrentPage = (pageNumber) => {
+    this.setState({
+      currentPage: pageNumber,
+    });
+  };
+
   render() {
+    const { empleados, empleadosSeleccionados, currentPage, itemsPerPage, searchTerm } = this.state;
+
+    // Filtrar empleados basados en el término de búsqueda
+    const filteredEmpleados = empleados.filter((empleado) =>
+      `${empleado.nombre} ${empleado.apellido} ${empleado.puesto} ${empleado.fechaNacimiento}`
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase())
+    );
+
+    // Calcula el índice del primer elemento en la página actual
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentEmpleados = filteredEmpleados.slice(indexOfFirstItem, indexOfLastItem);
+
     return (
       <div className="h-full max-h-screen p-6 overflow-x-auto border-gray-400 border-solid">
-        <div className="text-sm bg-white divide-y-2 divide-gray-200 overflow-x-auto overflow-y-auto shadow-2xl">
+        <div className="text-sm bg-white divide-y-2 divide-gray-200 overflow-x-auto overflow-y-auto shadow-md">
           <button
             type="submit"
-            className="inline-block px-4 py-2  mx-5 my-2 mt-4 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+            className="inline-block px-4 py-2 mx-5 my-2 mt-4 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded hover-bg-transparent hover-text-indigo-600 focus-outline-none focus-ring active-text-indigo-500"
           >
-            <Link to= "./AddEmployee">Agregar Empleado +</Link>
+            <Link to="./AddEmployee">Agregar Empleado +</Link>
           </button>
           <button
             onClick={this.handleSubmitEliminar}
-            className="inline-block px-4 py-2 mx-5 my-5 mt-4 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+            className="inline-block px-4 py-2 mx-5 my-5 mt-4 text-sm font-medium text-white bg-indigo-600 border border-indigo-600 rounded hover-bg-transparent hover-text-indigo-600 focus-outline-none focus-ring active-text-indigo-500"
           >
             Quitar empleado
           </button>
+          <input
+            type="text"
+            placeholder="Buscar empleado"
+            value={searchTerm}
+            onChange={this.handleSearch}
+            className="mx-6 my-6 mt-4 px-3 py-2 text-sm rounded border border-indigo-300 focus-outline-none focus-ring"
+          />
           <table>
             <thead>
               <tr>
@@ -61,28 +97,38 @@ class ListEmployee extends Component {
                   />
                 </th>
                 <th className="px-20 py-2">Nombre</th>
-                <th className="px-4 py-2">Puesto</th>
+                <th className="px-20 py-2">Apellido</th>
+                <th className="px-20 py-2">Puesto</th>
+                <th className="px-20 py-2">Fecha de Nacimiento</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.empleados.map((empleado, index) => (
+              {currentEmpleados.map((empleado, index) => (
                 <tr key={index}>
                   <td className="px-4 py-2">
                     <input
                       type="checkbox"
                       className="w-5 h-5 border-gray-300 rounded"
-                      checked={this.state.empleadosSeleccionados[index] || false}
+                      checked={empleadosSeleccionados[index] || false}
                       onChange={() => this.handleEliminarEmpleado(index)}
                     />
                   </td>
-                  <td className="px-4 py-2">
-                    {empleado.nombre} {empleado.apellido}
-                  </td>
+                  <td className="px-4 py-2">{empleado.nombre}</td>
+                  <td className="px-4 py-2">{empleado.apellido}</td>
                   <td className="px-4 py-2">{empleado.puesto}</td>
+                  <td className="px-4 py-2">{empleado.fechaNacimiento}</td>
                 </tr>
               ))}
             </tbody>
           </table>
+          <div className="my-5">
+            <Pagination
+              totalItems={filteredEmpleados.length}
+              itemsPerPage={itemsPerPage}
+              currentPage={currentPage}
+              setCurrentPage={this.setCurrentPage}
+            />
+          </div>
         </div>
       </div>
     );
